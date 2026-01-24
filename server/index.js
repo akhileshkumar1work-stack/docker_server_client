d3c78a7ebc2e3c914fd72845ea2fe1c2de843588
akhileshkumar1work-stack <akhilesh.1.work@gmail.com>
2026-01-25 00:31:50 +0530

    remove cors url

--- server/index.js
+++ server/index.js
@@ -5,16 +5,17 @@ const app = express();
 
 app.use(express.json());
 app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:5174",
-      "http://localhost:3000",
-      "http://46.28.44.193"
-    ],
-    credentials: true,
-  })
+  cors()
 );
+// {
+//     origin: [
+//       "http://localhost:5173",
+//       "http://localhost:5174",
+//       "http://localhost:3000",
+//       "http://46.28.44.193"
+//     ],
+//     credentials: true,
+//   }
 
 // API route
 app.get("/api/message", (req, res) => {
